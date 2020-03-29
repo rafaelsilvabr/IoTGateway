2ac469a94214626c568c202ab5ee9520b2c2bf60
Rafael Silva <r4faelrs@gmail.com>
2020-03-29 14:28:54 -0300

    1st commit

---
+++ mqtt_gateway.py
@@ -0,0 +1,32 @@
+import paho.mqtt.client as mqtt
+import json
+from register import register
+
+#define json header
+headers= {
+	'Content-type': 'application/json',
+}
+
+#define mqtt parameters
+MQTT_ADDRESS = "soldier.cloudmqtt.com"
+MQTT_PORT = 10514
+MQTT_TIMEOUT = 10
+TOPIC = "test4"
+client = mqtt.Client()
+
+#receives data from a sensor and sends it to the  InterSCity platform
+def on_message(client,userdata,msg):
+	print("Mensagem Recebida :")
+	print (msg.payload)
+	received_data = json.loads(msg.payload)
+	
+	#the register method registers and calls the send_data method
+	register(received_data)
+
+#mqtt connection
+client.username_pw_set("ctuqpqym","Xk5GNcWqcmZG")
+client.connect(MQTT_ADDRESS,MQTT_PORT,MQTT_TIMEOUT)
+client.subscribe(TOPIC)
+
+client.on_message = on_message
+client.loop_forever()
