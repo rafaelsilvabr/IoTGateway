6f6a2a2f9d9fb84a8141a55c0d1287a8c8b93250
Rafael Silva <r4faelrs@gmail.com>
2020-05-03 16:47:54 -0300

    implementation

--- src/protocolClients.py
+++ src/protocolClients.py
@@ -5,7 +5,6 @@ from sender import Sender
 import paho.mqtt.client as mqtt
 import timeit 
 
-
 class ProtocolClient (object):
 	def __init__(self,gatewayId):
 		self.gatewayId = gatewayId
@@ -29,7 +28,7 @@ class MqttClass (ProtocolClient):
 		print("Received a Message")
 		receivedData = json.loads(msg.payload)
 		
-		#check type od mesage, register or data
+		#check type register or pub
 		if(receivedData['registred']==False):
 			dbIds = self.reg.registerResourceIC(receivedData)
 			if(dbIds != False):
@@ -54,6 +53,37 @@ class MqttClass (ProtocolClient):
 		self.mqttClient.on_message = self.on_message	
 		self.mqttClient.loop_forever()
 
+class MqttClassWithoutReg (ProtocolClient):
+	def __init__(self, gatewayId, mqttAddress, mqttPort, mqttTimeout, mqttTopic):
+		ProtocolClient.__init__(self,gatewayId)
+		self.mqttAddress = mqttAddress
+		self.mqttPort = mqttPort
+		self.mqttTimeout = mqttTimeout
+		self.mqttTopic = mqttTopic
 
+	def on_message(self,client,userdata,msg):
+		start = timeit.timeit()
+		print("Received a Message")
+		receivedData = json.loads(msg.payload)
+		
+		#check type register or pub
+		dbIds = self.reg.registerResourceIC(receivedData)
+		if(dbIds != False):
+			#send data to IC
+			self.send.sendDataIC(receivedData)
+			
+		end = timeit.timeit()
+		print('Runtime:')
+		print(end - start)
+		print('\n')		
+
+	def startListening (self,mqttBrokerUser,mqttBrokerPassword):
+		self.mqttClient = mqtt.Client()
+		self.mqttClient.username_pw_set(mqttBrokerUser,mqttBrokerPassword)
+		self.mqttClient.connect(self.mqttAddress,self.mqttPort,self.mqttTimeout)
+		self.mqttClient.subscribe(self.mqttTopic)	
+		print('Connected')
+		self.mqttClient.on_message = self.on_message	
+		self.mqttClient.loop_forever()
 
 
